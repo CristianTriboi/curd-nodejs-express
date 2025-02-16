8feb5b86669ee02326502d1f0ceaae5cf3b6bc12
cristian <cristian.triboi@gmail.com>
2025-02-16 13:44:41 -0500

    implement some api call for creating, retriving, updating and deleting users

--- routes/users.js
+++ routes/users.js
@@ -21,39 +21,103 @@ let users = [
         email:"joyalwhite@gamil.com",
         DOB:"21-03-1989",
     },
+    {
+        firstName: "Jon",
+        lastName: "Lovato",
+        email: "jonlovato@theworld.com",
+        DOB: "10/10/1995"
+    }
 ];
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({users}, null, 4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    res.send(filtered_users);
+});
+
+router.get("/lastName/:lastName", (req, res) => {
+    // Extract the lastName parameter from the request URL
+    const lastName = req.params.lastName;
+    // Filter the users array to find users whose lastName matches the extracted lastName parameter
+    let filtered_lastname = users.filter((user) => user.lastName === lastName);
+    // Send the filtered_lastname array as the response to the client
+    res.send(filtered_lastname);
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users.push({
+        "firstName": req.query.firstName,
+        "lastName": req.query.lastName,
+        "email": req.query.email,
+        "DOB": req.query.DOB
+    });
+    // Send a success message as the response, indicating the user has been added
+    res.send("The user " + req.query.firstName + " has been added!\n");
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Extract email parameter and find users with matching email
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    
+    if (filtered_users.length > 0) {
+        let filtered_user = filtered_users[0];        
+        let firstName = req.query.firstName;
+        let lastName = req.query.lastName; 
+        let email = req.query.email; 
+        let DOB = req.query.DOB; 
+
+
+        if (firstName) {
+            filtered_user.firstName = firstName;
+        }
+
+        if (lastName) {
+            filtered_user.lastName = lastName;
+        }
+
+        if (lastName) {
+            filtered_user.lastName = lastName;
+        }
+
+        if (DOB) {
+            filtered_user.DOB = DOB;
+        }
+        
+        /*
+        Include similar code here for updating other attributes as needed
+        */
+        
+        // Replace old user entry with updated user
+        users = users.filter((user) => user.email != email);
+        users.push(filtered_user);
+        
+        // Send success message indicating the user has been updated
+        res.send(`User with the email ${email} updated.\n`);
+    } else {
+        // Send error message if no user found
+        res.send("Unable to find user!");
+    }
 });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+    // Filter the users array to exclude the user with the specified email
+    users = users.filter((user) => user.email != email);
+    // Send a success message as the response, indicating the user has been deleted
+    res.send(`User with the email ${email} deleted.`);
 });
 
 module.exports=router;
